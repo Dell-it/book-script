7669336cc2ed34fb0efe9e5151f99b061a9c860e
Dell-it <69255597+Dell-it@users.noreply.github.com>
2024-05-15 15:02:15 +0300

    Update book.user.js
    Add HD size images, add download progress

--- book.user.js
+++ book.user.js
@@ -4,9 +4,9 @@
 // @match       https://www.as-books.jp/trhtml5_ssl/view/*
 // @match       https://lib.as-books.jp/html5_ssl/view/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
-// @description 14.05.2024, 16:29:48
+// @description 15.05.2024, 15:00:00
 // @noframes
 // @run-at      document-idle
 // ==/UserScript==
@@ -17,11 +17,15 @@ const bookState = {
     iframe: null,
     printButtonEl: null,
     total: 0,
-    imagesFolder: '/books/images/2',
+    imagesFolder: '/books/images',
+    resolvedImages: [],
 }
 
-const getPrintButtonText = () =>
-    `Печать: ${bookState.total} стр.`
+const getPrintButtonDefaultText = () =>
+    `Распечатать: ${bookState.total} стр.`
+
+const getPrintButtonProgressText = (count) =>
+    `Загрузка... ${count} \/ ${bookState.total} стр.`
 
 function addPrintStyleInDoc(document) {
     const printStyles = `
@@ -30,14 +34,15 @@ function addPrintStyleInDoc(document) {
         size: auto;
         margin: 0mm;
       }
-    
+
       body {
         margin: 0;
         padding: 0;
       }
-    
+
       img {
         display: block;
+        max-width: 100%;
       }
     }
 `
@@ -47,31 +52,68 @@ function addPrintStyleInDoc(document) {
     document.body.appendChild(printStyleEl)
 }
 
+function getImagePath(quality, fileName) {
+    return `${bookState.baseFileHref}${bookState.imagesFolder}/${quality}/${fileName}.jpg`
+}
+
+async function checkImageAvailability() {
+    const qualities = [4, 3, 2, 1]; // Порядок качества от лучшего к худшему
+    const fileName = '1';
+
+    for (let i = 0; i < qualities.length; i++) {
+
+        const quality = qualities[i]
+        const path = getImagePath(quality, fileName);
+        const response = await fetch(path, {method: 'HEAD'});
+
+        if (response.ok && response.status !== 302 && response.headers.get('Content-type') !== 'text/html') {
+            console.log(`[book-to-PDF] Качество изображений: ${quality}\/${qualities[0]}.`)
+            return quality;
+        }
+    }
+
+    console.log(`[book-to-PDF] Файл ${fileName} отсутствует во всех качествах.`);
+    return null;
+}
+
 async function addImagesInDoc(document) {
-    const imageWrapperEl = document.createElement('div')
+    let imgLoadedCounter = 0;
+    const imageWrapperEl = document.createElement('div');
+
+    const quality = await checkImageAvailability();
+
+    if (!quality) {
+        console.error('[book-to-PDF] Проверочное изображение не найдено.')
+        return false
+    }
+
 
     const imagePaths =
-        [...new Array(bookState.total)].map((_, index) => `${bookState.baseFileHref}${bookState.imagesFolder}/${index + 1}.jpg`)
+        [...new Array(bookState.total)].map((_, index) => getImagePath(quality, index + 1));
 
     await Promise.all(imagePaths.map((path) =>
         new Promise((resolve, reject) => {
             const img = new Image();
             imageWrapperEl.appendChild(img)
-            img.onload = function () {
+            img.onload = () => {
+                bookState.printButtonEl.textContent = getPrintButtonProgressText(++imgLoadedCounter);
                 resolve(path);
             };
-            img.onerror = function () {
+            img.onerror = () => {
                 reject(path);
             };
             img.src = path;
         })
-    )).then(function (images) {
-        console.log("Все изображения успешно загружены.");
+    )).then(function () {
+        console.log("[book-to-PDF] Все изображения успешно загружены.");
     }).catch(function (error) {
-        console.error("Ошибка загрузки изображений:", error);
+        console.error("[book-to-PDF] Ошибка загрузки изображений:", error);
+        return false
     });
 
     document.body.appendChild(imageWrapperEl)
+
+    return true
 }
 
 window.App = new Proxy(window.App, {
@@ -84,7 +126,6 @@ window.App = new Proxy(window.App, {
     }
 });
 
-// Создаем bookState.iframe и добавляем сгенерированное содержимое к нему
 function createIframe(onAppend) {
     if (bookState.iframe) {
         onAppend(true)
@@ -92,30 +133,30 @@ function createIframe(onAppend) {
     }
 
     bookState.iframe = document.createElement('iframe');
-    bookState.iframe.src = 'about:blank'; // Загружаем пустую страницу
+    bookState.iframe.src = 'about:blank';
     bookState.iframe.hidden = true;
     bookState.iframe.onload = async () => {
         const iframeDoc = bookState.iframe.contentWindow.document;
 
-        await addImagesInDoc(iframeDoc)
+        const isCompleteImages = await addImagesInDoc(iframeDoc)
         addPrintStyleInDoc(iframeDoc)
 
-        onAppend(true)
+        onAppend(isCompleteImages)
     };
 
     bookState.iframe.onerror = (e) => {
         onAppend(false);
-        console.error(e);
+        console.error("[book-to-PDF] Ошибка создания iframe: ", e);
     }
 
     document.body.appendChild(bookState.iframe);
 }
 
 async function handlePrint() {
-    bookState.printButtonEl.textContent = 'Загрузка...'
+    bookState.printButtonEl.textContent = getPrintButtonProgressText(0)
 
     createIframe((status) => {
-        bookState.printButtonEl.textContent = getPrintButtonText()
+        bookState.printButtonEl.textContent = getPrintButtonDefaultText()
         status && bookState.iframe.contentWindow.print()
     })
 }
@@ -123,7 +164,7 @@ async function handlePrint() {
 
 function getPrintButton() {
     const button = document.createElement("button");
-    button.textContent = getPrintButtonText()
+    button.textContent = getPrintButtonDefaultText()
 
     Object.assign(button.style, {
         position: 'absolute',
@@ -133,7 +174,7 @@ function getPrintButton() {
         padding: "10px 20px",
         backgroundColor: "#007bff",
         color: "#fff",
-        width: '200px',
+        width: '300px',
         border: "none",
         borderRadius: "5px",
         cursor: "pointer",
@@ -160,11 +201,8 @@ async function initBookPrinter(book) {
     // const total = Number(xml?.match(/<total>(.*?)<\/total>/i)?.[1]) || null
     bookState.total = book?.lastPage || 0;
 
-    // console.log(`Найдено страниц: ${bookState.total}`);
-
     if (!bookState.total) return
 
     bookState.printButtonEl = getPrintButton()
     document.body.appendChild(bookState.printButtonEl);
 }
-
